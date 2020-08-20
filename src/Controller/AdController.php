3c6f60826dc8caab4b64a3a74f19054a98cab772
Mikaia Ralaivta <mikaiajonathan@gmail.com>
2020-08-20 23:55:20 +0200

    🎉 Finish form chapter 2

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -30,7 +30,7 @@ class AdController extends AbstractController
      *
      * @return Response
      */
-    public function create(Request $request, ObjectManager $objectManager): HttpFoundationResponse
+    public function create(Request $request, ObjectManager $manager): HttpFoundationResponse
     {
         $ad = new Ad();
         $form = $this->createForm(AdType::class, $ad);
@@ -38,8 +38,13 @@ class AdController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $objectManager->persist($ad);
-            $objectManager->flush();
+            foreach ($ad->getImages() as $image) {
+                $image->setAd($ad);
+                $manager->persist($image);
+            }
+
+            $manager->persist($ad);
+            $manager->flush();
 
             $this->addFlash("success", "L'annonce {{$ad->getTitle()}} a bien été ajouté");
 
@@ -49,6 +54,45 @@ class AdController extends AbstractController
         }
 
         return $this->render("ad/new.html.twig", ["form" => $form->createView()]);
+    }
+
+    /**
+     * Permet d'afficher le formulaire d'édition
+     *
+     * @Route("/ads/{slug}/edit", name="ads_edit")
+     * 
+     * @return Response
+     */
+    public function edit(Ad $ad, Request $request, ObjectManager $manager): HttpFoundationResponse
+    {
+
+        $form = $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            foreach ($ad->getImages() as $image) {
+                $image->setAd($ad);
+                $manager->persist($image);
+            }
+
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Les modifications de l'annonce <strong>{$ad->getTitle()}</strong> ont bien été enregistrées !"
+            );
+
+            return $this->redirectToRoute('ads_show', [
+                'slug' => $ad->getSlug()
+            ]);
+        }
+
+        return $this->render('ad/edit.html.twig', [
+            'form' => $form->createView(),
+            'ad' => $ad
+        ]);
     }
 
     /**
--- src/Form/AdType.php
+++ src/Form/AdType.php
@@ -3,42 +3,70 @@
 namespace App\Form;
 
 use App\Entity\Ad;
+use App\Form\ImageType;
+use App\Form\ApplicationType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\MoneyType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
-class AdType extends AbstractType
+class AdType extends ApplicationType
 {
-    /**
-     * @param [String] $title
-     * @param [String] $placeholder
-     * @return Array
-     */
-    private function getConfiguration($title, $placeholder)
-    {
-        return [
-            'label' => $title,
-            'attr' => [
-                'placeholder' => $placeholder,
-            ],
-        ];
-    }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', TextType::class, $this->getConfiguration("Titre", "Titre de l'annonce"))
-            ->add('slug', TextType::class, $this->getConfiguration("Chaine url", "Automatique"))
-            ->add('coverImage', UrlType::class, $this->getConfiguration("Url de l'image", "Donnez l'adresse de l'image"))
-            ->add('introduction', TextType::class, $this->getConfiguration("Introduction", "Donnez une description"))
-            ->add('content', TextareaType::class, $this->getConfiguration("Contenu", "Description détaillé"))
-            ->add('price', MoneyType::class, $this->getConfiguration("Prix", "Prix du bien"))
-            ->add('rooms', IntegerType::class, $this->getConfiguration("Nombre de chambre", "Entrez le nombre de chambre"));
+            ->add(
+                'title',
+                TextType::class,
+                $this->getConfiguration("Titre", "Tapez un super titre pour votre annonce")
+            )
+            ->add(
+                'slug',
+                TextType::class,
+                $this->getConfiguration("Adresse web", "Tapez l'adresse web (automatique)", [
+                    'required' => false
+                ])
+            )
+            ->add(
+                'coverImage',
+                UrlType::class,
+                $this->getConfiguration("URL de l'image principale", "Donnez l'adresse d'une image qui donne vraiment envie")
+            )
+            ->add(
+                'introduction',
+                TextType::class,
+                $this->getConfiguration("Introduction", "Donnez une description globale de l'annonce")
+            )
+            ->add(
+                'content',
+                TextareaType::class,
+                $this->getConfiguration("Description détaillée", "Tapez une description qui donne vraiment envie de venir chez vous !")
+            )
+            ->add(
+                'rooms',
+                IntegerType::class,
+                $this->getConfiguration("Nombre de chambres", "Le nombre de chambres disponibles")
+            )
+            ->add(
+                'price',
+                MoneyType::class,
+                $this->getConfiguration("Prix par nuit", "Indiquez le prix que vous voulez pour une nuit")
+            )
+            ->add(
+                'images',
+                CollectionType::class,
+                [
+                    'entry_type' => ImageType::class,
+                    'allow_add' => true,
+                    'allow_delete' => true
+                ]
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver)
